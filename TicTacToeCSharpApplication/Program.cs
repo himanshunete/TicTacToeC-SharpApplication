3b41528b15646afd4a2d51202a1dd90eaccf2ed2
himanshunete <himanshuneteh@gmail.com>
2021-02-07 11:18:13 +0530

    [himanshunete]Add CreateBoard

--- TicTacToeCSharpApplication/Program.cs
+++ TicTacToeCSharpApplication/Program.cs
@@ -2,11 +2,25 @@
 
 namespace TicTacToeCSharpApplication
 {
+    class TicTactoe
+    {
+        public static string PLAYER_1;
+        public static char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        public static void createBoard()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(" Empty board:" + i + ": " + board[i]);
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" Welcome to Tic Tac Toe Game");
+            TicTactoe.createBoard();
         }
     }
 }
